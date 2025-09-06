13f4ca1965237e96297c1dce8687d1e81e3333ab
Tommy Yu <tommy.yu@auckland.ac.nz>
2025-09-06 15:45:52 +1200

    Correct coverage

--- src/pmr2/wfctrl/__init__.py
+++ src/pmr2/wfctrl/__init__.py
@@ -2,5 +2,5 @@ from importlib import metadata
 
 try:
     __version__ = metadata.version('pmr2.wfctrl')
-except metadata.PackageNotFoundError:
+except metadata.PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
